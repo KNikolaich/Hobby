d0374d1e07804e2db6c2c142642abc55282a0eec
Kirill <kirill.0312@mail.ru>
2017-10-10 08:46:53 +0300

    Состояние дел на 10 ноября
    Есть кнопка переключения подсветки Авто/выкл/вкл
    Есть силовое управление движком
    Есть отдельно часы (на цыфровых пинах) с дисплеем

---
+++ sketch_oct09b/sketch_oct09b.ino
@@ -0,0 +1,74 @@
+#include <Wire.h>
+#include <TimeLib.h>
+#include <DS1307RTC.h>
+#include "TM1637.h"
+
+const int CLK = 11; //Set the CLK pin connection to the display
+const int DIO = 12; //Set the DIO pin connection to the display
+int NumStep = 0;  //Variable to interate
+int8_t TimeDisp[] = {0x00,0x00,0x00,0x00};
+
+bool point = false;
+TM1637 tm1637(CLK, DIO);  //set up the 4-Digit Display.
+
+void setup() {
+  
+  tm1637.set();
+  tm1637.init();
+  
+  Serial.begin(9600);
+  while (!Serial) ; // wait for serial
+  delay(200);
+  Serial.println("DS1307RTC Read Test");
+  Serial.println("-------------------");
+}
+
+void loop() {
+  tmElements_t tm;
+
+  if (RTC.read(tm)) {
+    Serial.print("Ok, Time = ");
+    print2digits(tm.Hour);
+    Serial.write(':');
+    print2digits(tm.Minute);
+    Serial.write(':');
+    print2digits(tm.Second);
+    Serial.print(", Date (D/M/Y) = ");
+    Serial.print(tm.Day);
+    Serial.write('/');
+    Serial.print(tm.Month);
+    Serial.write('/');
+    Serial.print(tmYearToCalendar(tm.Year));
+    Serial.println();
+    
+if(tm.Second % 2 == 0)tm1637.point(POINT_ON);
+  else tm1637.point(POINT_OFF); 
+    //display.setSegments(tm.Second % 2 ? 1 : 0);
+
+      TimeDisp[0] = tm.Hour / 10;
+  TimeDisp[1] = tm.Hour % 10;
+  TimeDisp[2] = tm.Minute / 10;
+  TimeDisp[3] = tm.Minute % 10;
+    
+  } else {
+    if (RTC.chipPresent()) {
+      Serial.println("The DS1307 is stopped.  Please run the SetTime");
+      Serial.println("example to initialize the time and begin running.");
+      Serial.println();
+    } else {
+      Serial.println("DS1307 read error!  Please check the circuitry.");
+      Serial.println();
+    }
+    //delay(500);  //A half second delay between steps.
+
+    delay(9000);
+  }
+  delay(500);
+}
+
+void print2digits(int number) {
+  if (number >= 0 && number < 10) {
+    Serial.write('0');
+  }
+  Serial.print(number);
+}
---
+++ sketch_oct10a_clock_on/sketch_oct10a_clock_on.ino
@@ -0,0 +1,46 @@
+// часы
+#include <DS1302.h>
+DS1302 rtc(10, 7, 5);
+
+// дисплей
+#include "TM1637.h"
+
+const int CLK = 11; //Set the CLK pin connection to the display
+const int DIO = 12; //Set the DIO pin connection to the display
+int8_t TimeDisp[] = {0x00,0x00,0x00,0x00};
+
+TM1637 tm1637(CLK, DIO);  //set up the 4-Digit Display.
+
+
+void setup()
+{
+  
+  tm1637.set();
+  tm1637.init();
+    Serial.begin(9600);
+    //display.setBrightness(0x0a);  //set the diplay to maximum brightness
+}
+
+
+void loop()
+{
+    Time t = rtc.time();
+    Serial.println(t.yr);
+    Serial.println(t.hr);
+    Serial.println(t.min);
+    Serial.println(t.sec);
+    Serial.println(t.mon);
+    Serial.println(t.date);
+    Serial.println(t.day);
+
+if(t.sec % 2 == 0)tm1637.point(POINT_ON);
+  else tm1637.point(POINT_OFF); 
+    //display.setSegments(tm.Second % 2 ? 1 : 0);
+
+      TimeDisp[0] = t.hr / 10;
+  TimeDisp[1] = t.hr % 10;
+  TimeDisp[2] = t.min / 10;
+  TimeDisp[3] = t.min % 10;
+  tm1637.display(TimeDisp);
+    delay(500);
+}
