4203930fdf8d432dcb47a4cf6d192468205d5a53
KNikolai4 <kirill.0312@mail>
2017-10-11 09:13:05 +0300

    переделка под экономичный для ардуино режим Объединение проектов с часами и светодиодами

--- sketch_InfraLed_water/sketch_InfraLed_water.ino/sketch_InfraLed_water.ino.ino
+++ sketch_InfraLed_water/sketch_InfraLed_water.ino/sketch_InfraLed_water.ino.ino
@@ -8,6 +8,28 @@
 #define ledR 6 // LED red pin D6
 #define ledY 8 // LED yellow pin D8
 
+// часы
+#include <DS1302.h>
+#include <stdio.h>
+
+// дисплей
+#include "TM1637.h"
+
+// часы
+DS1302 rtc(10, 7, 5);
+
+// дисплей
+
+#define interval 1000           // интервал между переключениями дисплея
+
+#define CLK 11 //Set the CLK pin connection to the display
+#define DIO 12 //Set the DIO pin connection to the display
+int8_t TimeDisp[] = {0x0e,0x0f,0x0f,0x0f};
+unsigned long previousMillis = 0;        // храним время последнего переключения дисплея
+bool points = false;
+
+TM1637 tm1637(CLK, DIO);  //set up the 4-Digit Display.
+
 int btnInput = 4; // Input managment channel pin 4 (4)
 volatile long CH1PulseStartTicks;
 bool ledState = false;
@@ -15,6 +37,11 @@ byte triadeState = 0;
 bool button_old = false;
 
 void setup() {
+
+  // дисплей
+  tm1637.set();
+  tm1637.init();
+  
   // put your setup code here, to run once:
   pinMode(sens1, INPUT);
   pinMode(btnInput, INPUT_PULLUP);
@@ -36,7 +63,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  RefreshDateTime();
   int val1 = analogRead(sens1);//ApproximateSensor();
   bool valBtn = digitalRead(btnInput);
   
@@ -137,4 +164,60 @@ if (ledState){
   CH1PulseStartTicks = micros();
 }
 
+
+// Работы с дисплеем
+void RefreshDateTime(){
+  unsigned long currentMillis = millis();
+ //проверяем не прошел ли нужный интервал, если прошел то
+  if(currentMillis - previousMillis > interval) {
+    // сохраняем время последнего переключения
+    previousMillis = currentMillis; 
+
+    Time tm = printTime();
+
+    tm1637.point(points ? POINT_ON:POINT_OFF);
+    points = !points;
+      
+    TimeDisp[0] = tm.hr / 10;
+    TimeDisp[1] = tm.hr % 10;
+    TimeDisp[2] = tm.min / 10;
+    TimeDisp[3] = tm.min % 10;
+    tm1637.display(TimeDisp);  
+  
+    
+  }
+}
+
+Time printTime() {
+  // Get the current time and date from the chip.
+  Time t = rtc.time();
+
+  // Name the day of the week.
+  const String day = dayAsString(t.day);
+
+  // Format the time and date and insert into the temporary buffer.
+  char buf[50];
+  snprintf(buf, sizeof(buf), "%s %04d-%02d-%02d %02d:%02d:%02d",
+           day.c_str(),
+           t.yr, t.mon, t.date,
+           t.hr, t.min, t.sec);
+
+  // Print the formatted string to serial so we can see the time.
+  Serial.println(buf);
+  return t;
+}
+
+
+String dayAsString(const Time::Day day) {
+  switch (day) {
+    case Time::kSunday: return "Sunday";
+    case Time::kMonday: return "Monday";
+    case Time::kTuesday: return "Tuesday";
+    case Time::kWednesday: return "Wednesday";
+    case Time::kThursday: return "Thursday";
+    case Time::kFriday: return "Friday";
+    case Time::kSaturday: return "Saturday";
+  }
+  return "(unknown day)";
+}
 
